951bb9f6754cade85ec56d7e5878f7148ec1c1e5
ApurvChaturOfficial <apurvchaturoffiial@gmail.com>
2024-01-10 12:36:49 +0530

    Chappa Chappa

--- site/admin/vite.config.js
+++ site/admin/vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv  } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
-import VitePluginHtmlEnv from 'vite-plugin-html-env'
 
 
 // https://vitejs.dev/config/
@@ -413,7 +412,6 @@ export default defineConfig(({ mode }) => {
       VitePWA({
         manifest: MANIFEST_DATA
       }),
-      VitePluginHtmlEnv(),
     ],
     resolve: {
       alias: {
