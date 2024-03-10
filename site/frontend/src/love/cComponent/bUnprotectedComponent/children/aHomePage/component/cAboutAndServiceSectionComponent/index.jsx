debcae774d9d7d8a2a067c0bc0f19357f2a2593c
ApurvChatur <chaturapurv@gmail.com>
2024-03-10 08:25:33 +0530

    Cool

--- site/frontend/src/love/cComponent/bUnprotectedComponent/children/aHomePage/component/cAboutAndServiceSectionComponent/index.jsx
+++ site/frontend/src/love/cComponent/bUnprotectedComponent/children/aHomePage/component/cAboutAndServiceSectionComponent/index.jsx
@@ -1,16 +1,13 @@
+import React from "react";
 import AboutSectionComponent from "./component/aAboutSectionComponent";
 import ServiceSectionComponent from "./component/bServiceSectionComponent";
 
 function AboutAndServiceSectionComponent({ Redux }) {
   return (
-    <MKBox component="section" py={6} my={6}>
-      <Container>
-        <Grid container item xs={11} spacing={3} alignItems="center" sx={{ mx: "auto" }}>
-          <AboutSectionComponent Redux={Redux} />
-          <ServiceSectionComponent Redux={Redux} />
-        </Grid>
-      </Container>
-    </MKBox>
+    <React.Fragment>
+      <AboutSectionComponent Redux={Redux} />
+      <ServiceSectionComponent Redux={Redux} />
+    </React.Fragment>
   );
 }
 
